863d4c32805b64ac7e13096b9594e2dd289e911e
burakcnaksy0 <aksoyburak808@gmail.com>
2026-02-04 16:32:08 +0300

    Initializes the project - added the dependencies.

---
+++ src/main/java/com/burakcanaksoy/healthtracking/HealthTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.burakcanaksoy.healthtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burakcanaksoy/healthtracking/HealthTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakcanaksoy.healthtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
